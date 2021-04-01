daaf99a7139f19eeecdee6293793ce35a8d49e36
matsumoto-keita <matsukei88@gmail.com>
2021-04-01 11:52:17 +0900

    輪郭の表示

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 $(function(){
-// 準備
 var canvas = new fabric.Canvas('cnvs');
-// 四角
-canvas.add(
-	new fabric.Rect({
-		width: 100,
-		height: 200,
-		left: 100,
-		top: 100,
-		fill: 'red',
-	}));
+
+fabric.Image.fromURL('face1.png',function(oImg){
+	oImg.set({
+		hasRotationPoint: false,
+		hasControls: false,
+		selectable: false,
+	});
+	canvas.add(oImg);
+});
+fabric.Image.fromURL('face1.png',function(oImg){
+	oImg.scaleToWidth(100);
+	canvas.add(oImg);
+});
 });
